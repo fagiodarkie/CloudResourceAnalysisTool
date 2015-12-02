2a6c4c89d6f4d01c6d4309696bf8ccac40aa4872
fagio-darkie <fagio.darkie@gmail.com>
2015-12-02 23:14:36 +0100

    Update bytecode tests.

---
+++ src/tests/sources/Test1.java
@@ -0,0 +1,17 @@
+package tests.sources;
+public class Test1 {
+
+	public void main(String[] args) {
+	
+		int n = 3;
+		String a = "";
+		while (n != 0) {
+		
+			a = new String("");
+			n--;
+		
+		}
+	
+	}
+
+}
---
+++ src/tests/sources/TestVM1.java
@@ -0,0 +1,32 @@
+package tests.sources;
+public class TestVM1 {
+
+	private class VM {
+	}
+
+	public VM createVM() {
+		return new VM();
+	}
+	
+	public void releaseVM(VM x) {
+	
+	}
+
+	public void main(String[] args) {
+	
+		int n = 3;
+		VM a = createVM();
+		while (n != 0) {
+		
+			releaseVM(a);
+			VM b = createVM();
+			a = createVM();
+			releaseVM(b);
+						
+			n--;
+		
+		}
+	
+	}
+
+}
